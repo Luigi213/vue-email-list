55db34382199d6df6e62a5b104a214b9b271e526
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-09 14:35:33 +0100

    bonus done

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -6,13 +6,13 @@ const {
 createApp({
     data() {
         return {
-            mail: []
+            mail: [],
         }
     },
     mounted() {
         for(let i=0; i<10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                this.mail.push(result.data.response);
+                this.mail.push(result.data.response)
             })
         }
     }
