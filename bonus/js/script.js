2cbfb84bb665080d8a9f65b9ebf2e81f819dcd18
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-09 13:39:40 +0100

    Add bonus folder

---
+++ bonus/js/script.js
@@ -0,0 +1,19 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+const {
+    createApp
+} = Vue;
+
+createApp({
+    data() {
+        return {
+            mail: []
+        }
+    },
+    mounted() {
+        for(let i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.mail.push(result.data.response);
+            })
+        }
+    }
+}).mount('#app')
